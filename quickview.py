de58f37c4606e1631f47f8da864442687c692781
mckib2 <nicholas.bgp@gmail.com>
2018-01-03 18:57:37 -0700

    quickview now uses the simple command line option reader and has core functionality

--- quickview.py
+++ quickview.py
@@ -1,14 +1,41 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from decode_opts import decode_simple_opts
+import sys
 
-def quickview(filename):
+def quickview(filename,
+              noIFFT=False):
+    '''
+    Display processed MRI data from .npz file.
+
+    No arguments displays the IFFT of the k-space data.
+
+    Command-line Options:
+    -nifft (no IFFT)
+                 Display k-space data, log magnitude and phase plots.
+
+    '''      
     data = np.load(filename)
-    print(data.keys())
-    print(data['kSpace'].shape)
     
-    #plt.plot()
-    #plt.title('test')
-    #plt.show()
+    # Average over all the averages, use first coil
+    coil = 0
+    num_avgs = data['kSpace'].shape[2]
+    avg = (np.squeeze(np.sum(data['kSpace'],axis=2))/num_avgs)[:,:,coil]
+
+    if noIFFT is False:
+        imData = np.fft.ifftshift(np.fft.ifft2(avg))
+        plt.imshow(np.absolute(imData),cmap='gray')
+        plt.title('Image Data')
+    else:
+        mag = np.log(np.absolute(avg))
+        phase = np.angle(avg)
+        f,(ax1, ax2) = plt.subplots(1,2,sharey=True)
+        ax1.imshow(mag,cmap='gray')
+        ax2.imshow(phase,cmap='gray')
+        ax1.set_title('log(Magnitude)')
+        ax2.set_title('Phase')
+
+    plt.show()
 
 if __name__ == '__main__':
-    quickview('test-data/test_Kspace.npz')
+    decode_simple_opts({ '-nifft':['noIFFT',False] },sys.argv[1:],quickview)
