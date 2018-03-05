4d0a552059c6180ddcd37d71ffd4c5be1a1c956d
mckib2 <nicholas.bgp@gmail.com>
2018-03-05 00:14:43 -0700

    cleanraw now opens file as binary to conform to latest numpy package and python3.

--- cleanraw.py
+++ cleanraw.py
@@ -3,39 +3,39 @@ import numpy as np
 
 def cleanraw(filename):
     # Give some test data
-    with open(filename,'r') as f:
-        num = np.fromfile(f,dtype=np.int32,count=1)
+    with open(filename,'rb') as f:
+        num = np.fromfile(f,dtype=np.int32,count=1)[0]
         info = f.read(num-4)
 
     # Unfortunately Windows exists
-    info = info.replace('\r',' ').replace('\0',' ')
+    info = info.replace(b'\r',b' ').replace(b'\0',b' ')
     
     # we want to ignore some junk before the <XProtocol>
-    start = info.find('<XProtocol>') - 1
+    start = info.find(b'<XProtocol>') - 1
     info = info[start:]
         
     # We get the same kind of junk before <XProtocol> and after Dicom
-    trouble = re.search(r'Dicom.*<XProtocol>',info)
-    info = info.replace(info[trouble.start():trouble.end()],'<Dicom> <XProtocol>')
+    trouble = re.search(r'Dicom.*<XProtocol>'.encode(),info)
+    info = info.replace(info[trouble.start():trouble.end()],b'<Dicom> <XProtocol>')
         
     # And again with Meas...<XProtocol>
-    trouble = re.search(r'.Meas.*<XProtocol>',info)
-    info = info.replace(info[trouble.start():trouble.end()],'<Meas> <XProtocol>')
+    trouble = re.search(r'.Meas.*<XProtocol>'.encode(),info)
+    info = info.replace(info[trouble.start():trouble.end()],b'<Meas> <XProtocol>')
         
     # Guess what - with MeasYaps...### ASC..., too
-    trouble = re.search(r'.MeasYaps.*###',info)
-    info = info.replace(info[trouble.start():trouble.end() - len('###')],'\n <MeasYaps>')
+    trouble = re.search(r'.MeasYaps.*###'.encode(),info)
+    info = info.replace(info[trouble.start():trouble.end() - len('###')],b'\n <MeasYaps>')
         
     # Found another...
-    trouble = re.search(r'.Phoenix.*<XProtocol>',info)
-    info = info.replace(info[trouble.start():trouble.end()],'<Phoenix> <XProtocol>')
+    trouble = re.search(r'.Phoenix.*<XProtocol>'.encode(),info)
+    info = info.replace(info[trouble.start():trouble.end()],b'<Phoenix> <XProtocol>')
         
     # This time we're missing a breakline
-    trouble = re.search(r'.Spice.*<XProtocol>',info)
-    info = info.replace(info[trouble.start():trouble.end()],'\n<Spice> <XProtocol>\n')
+    trouble = re.search(r'.Spice.*<XProtocol>'.encode(),info)
+    info = info.replace(info[trouble.start():trouble.end()],b'\n<Spice> <XProtocol>\n')
         
     # Get rid of everything at the end we don't want right now
-    end = info.rfind('}') + 1
+    end = info.rfind(b'}') + 1
     info = info[1:end]
 
     return(info)
