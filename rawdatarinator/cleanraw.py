900410943baf149a57295da5dd456108af235c6f
mckib2 <nicholas.bgp@gmail.com>
2018-06-28 19:59:26 -0600

    Must check to see if regex is matched before calling methods on results in cleanraw.py.:

--- rawdatarinator/cleanraw.py
+++ rawdatarinator/cleanraw.py
@@ -9,31 +9,36 @@ def cleanraw(filename):
 
     # Unfortunately Windows exists
     info = info.replace(b'\r',b' ').replace(b'\0',b' ')
-    
+
     # we want to ignore some junk before the <XProtocol>
     start = info.find(b'<XProtocol>') - 1
     info = info[start:]
-        
+
     # We get the same kind of junk before <XProtocol> and after Dicom
     trouble = re.search(r'Dicom.*<XProtocol>'.encode(),info)
-    info = info.replace(info[trouble.start():trouble.end()],b'<Dicom> <XProtocol>')
-        
+    if trouble is not None:
+        info = info.replace(info[trouble.start():trouble.end()],b'<Dicom> <XProtocol>')
+
     # And again with Meas...<XProtocol>
     trouble = re.search(r'.Meas.*<XProtocol>'.encode(),info)
-    info = info.replace(info[trouble.start():trouble.end()],b'<Meas> <XProtocol>')
-        
+    if trouble is not None:
+        info = info.replace(info[trouble.start():trouble.end()],b'<Meas> <XProtocol>')
+
     # Guess what - with MeasYaps...### ASC..., too
     trouble = re.search(r'.MeasYaps.*###'.encode(),info)
-    info = info.replace(info[trouble.start():trouble.end() - len('###')],b'\n <MeasYaps>')
-        
+    if trouble is not None:
+        info = info.replace(info[trouble.start():trouble.end() - len('###')],b'\n <MeasYaps>')
+
     # Found another...
     trouble = re.search(r'.Phoenix.*<XProtocol>'.encode(),info)
-    info = info.replace(info[trouble.start():trouble.end()],b'<Phoenix> <XProtocol>')
-        
+    if trouble is not None:
+        info = info.replace(info[trouble.start():trouble.end()],b'<Phoenix> <XProtocol>')
+
     # This time we're missing a breakline
     trouble = re.search(r'.Spice.*<XProtocol>'.encode(),info)
-    info = info.replace(info[trouble.start():trouble.end()],b'\n<Spice> <XProtocol>\n')
-        
+    if trouble is not None:
+        info = info.replace(info[trouble.start():trouble.end()],b'\n<Spice> <XProtocol>\n')
+
     # Get rid of everything at the end we don't want right now
     end = info.rfind(b'}') + 1
     info = info[1:end]
