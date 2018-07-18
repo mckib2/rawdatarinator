3593cb0b639e67a5fc5edf0afff798050bbbd705
mckib2 <nicholas.bgp@gmail.com>
2018-07-18 11:32:57 -0600

    Changes and version updated.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='rawdatarinator',
-    version='0.1.4',
+    version='0.1.5',
     author='Nicholas McKibben',
     author_email='mri@byu.edu',
     packages=find_packages(exclude=['test-data']),
