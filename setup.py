accd0b02784f6804b8e8ae1d3f30b8476bb9e933
Nicholas <nicholas.bgp@gmail.com>
2018-11-20 20:22:05 -0700

    Updated requirements to allow newer numpy,scipy,matplotlib,h5py.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='rawdatarinator',
-    version='0.1.8',
+    version='0.1.9',
     author='Nicholas McKibben',
     author_email='mri@byu.edu',
     packages=find_packages(exclude=['test-data']),
@@ -13,11 +13,11 @@ setup(
     description='Read raw data from Siemens MRI scanners with IDEA VB15.',
     long_description=open('README.rst').read(),
     install_requires=[
-        "numpy==1.14.1",
-        "scipy==1.0.0",
+        "numpy>=1.14.1",
+        "scipy>=1.0.0",
         "regex==2017.12.12",
-        "h5py==2.7.1",
-        "matplotlib==2.1.1",
+        "h5py>=2.7.1",
+        "matplotlib>=2.1.1",
         "ply==3.10"
     ],
     python_requires='>=2.7',
