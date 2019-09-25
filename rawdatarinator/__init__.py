9e05b9a2051fa2a00ba071a3b1695394c361a4c8
mckib2 <nicholas.bgp@gmail.com>
2019-09-25 17:20:24 -0600

    bring cython results to the module level

---
+++ rawdatarinator/__init__.py
@@ -0,0 +1,5 @@
+'''Bring functions up to correct level.'''
+
+from .twixread import twixread # pylint: disable=E0611
+from .read import read # pylint: disable=E0611
+from .write import write # pylint: disable=E0611
