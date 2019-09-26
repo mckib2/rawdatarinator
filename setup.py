c7cf9602286d0de1bb1979877c1ee76cb79eda35
mckib2 <nicholas.bgp@gmail.com>
2019-09-25 18:58:31 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ extensions = [
 
 setup(
     name='rawdatarinator',
-    version='1.1.1',
+    version='1.2.0',
     author='Nicholas McKibben',
     author_email='nicholas.bgp@gmail.com',
     packages=find_packages(),
