fb91f7fa505529b7cf0bdcae4d6c1576f0519134
mckib2 <nicholas.bgp@gmail.com>
2020-01-11 19:44:06 -0700

    ref issue #12, add preprocessor for MinGW compilation.

--- setup.py
+++ setup.py
@@ -5,14 +5,32 @@ from os import getenv
 from setuptools import setup, Extension, find_packages
 from setuptools.command.build_ext import build_ext as _build_ext
 
+from src.preprocessors import mingw_preprocessor
+
 copts = {
     'unix': ['-O3', '-fopenmp'],
     'mingw32': ['-O3'],
 }
 lopts = {
     'unix': ['-fopenmp'],
-    'mingw32': []
+    'mingw32': [],
 }
+
+# Here are all the files we pull from BART
+bart_files = [
+    "bart/src/misc/version.c",
+    "bart/src/num/vecops.c",
+    "bart/src/num/simplex.c",
+    "bart/src/num/optimize.c",
+    "bart/src/num/multind.c",
+    "bart/src/misc/ya_getopt.c",
+    "bart/src/misc/opts.c",
+    "bart/src/misc/misc.c",
+    "bart/src/misc/io.c",
+    "bart/src/misc/mmio.c",
+    "bart/src/misc/debug.c",
+    "bart/src/twixread.c",
+]
 
 class build_ext(_build_ext):
     '''Subclass build_ext to bootstrap numpy and deal with compile.'''
@@ -34,6 +52,10 @@ class build_ext(_build_ext):
             for e in self.extensions:
                 e.extra_link_args = lopts[c]
 
+        # We might need to do some preprocessing before we build...
+        if c in ['mingw32']:
+            for b in bart_files:
+                mingw_preprocessor(b)
 
         _build_ext.build_extensions(self)
 
@@ -48,21 +70,7 @@ if getenv('__DO_CYTHON_BUILD') == '1':
 extensions = [
     Extension(
         'rawdatarinator.twixread',
-        [
-            "bart/src/misc/version.c",
-            "bart/src/num/vecops.c",
-            "bart/src/num/simplex.c",
-            "bart/src/num/optimize.c",
-            "bart/src/num/multind.c",
-            "bart/src/misc/ya_getopt.c",
-            "bart/src/misc/opts.c",
-            "bart/src/misc/misc.c",
-            "bart/src/misc/io.c",
-            "bart/src/misc/mmio.c",
-            "bart/src/misc/debug.c",
-            "bart/src/twixread.c",
-            "src/twixread_pyx.%s" % pyx_or_c
-        ],
+        bart_files + ["src/twixread_pyx.%s" % pyx_or_c],
         include_dirs=['src/', 'bart/src/'],
         # extra_compile_args=['-O3'],
     ),
